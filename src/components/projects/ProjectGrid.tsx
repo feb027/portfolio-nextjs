3298fc5ff5e87159bb2c560b4348898f4756af0a
Feb07 <febnawanrochman2@gmail.com>
2025-02-06 00:44:37 +0700

    Remove index parameter from ProjectGrid map function

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -36,7 +36,7 @@ const ProjectGrid: FC<ProjectGridProps> = ({
   return (
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
-        {currentProjects.map((project, idx) => (
+        {currentProjects.map((project) => (
           <div 
             key={`${currentPage}-${project.title}`}
             onClick={() => onProjectSelect(project)}
